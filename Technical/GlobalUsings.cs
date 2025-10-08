75e89742d348c7de0ce066d9aadadd98f2fdf4c8
Sergei Shvets <justserega@gmail.com>
2025-10-08 13:22:09 +0700

    Use crossplatform types

--- Technical/GlobalUsings.cs
+++ Technical/GlobalUsings.cs
@@ -2,6 +2,10 @@
 
 global using CrossColor = System.Drawing.Color;
 global using CrossKey = Avalonia.Input.Key;
+global using CrossPen = Utils.Common.UniversalPen;
+global using CrossPens = Utils.Common.UniversalPens;
+global using CrossSolidBrush = Utils.Common.UniversalSolidBrush;
+global using CrossBrushes = Utils.Common.UniversalBrushes;
 global using CrossColors = System.Drawing.Color;
 global using CrossKeyEventArgs = Avalonia.Input.KeyEventArgs;
 
@@ -9,6 +13,10 @@ global using CrossKeyEventArgs = Avalonia.Input.KeyEventArgs;
 
 global using CrossColor = System.Windows.Media.Color;
 global using CrossKey = System.Windows.Input.Key;
+global using CrossPen = System.Drawing.Pen;
+global using CrossPens = System.Drawing.Pens;
+global using CrossSolidBrush = System.Drawing.SolidBrush;
+global using CrossBrushes = System.Drawing.Brushes;
 global using CrossColors = System.Windows.Media.Colors;
 global using CrossKeyEventArgs = System.Windows.Input.KeyEventArgs;
 
