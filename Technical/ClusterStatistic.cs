d702f6b116be53bdb5f8b5dfaa6d402e594176c5
AlbertoAmadorBelchistim <belchistim@gmail.com>
2026-01-10 18:01:03 +0100

    fix(clusterstatistic): seed SoT live window in chronological order

--- Technical/ClusterStatistic.cs
+++ Technical/ClusterStatistic.cs
@@ -2280,6 +2280,8 @@ public class ClusterStatistic : Indicator
         _rtPeakVolPerSec = 0m;
         _rtPeakDeltaPerSec = 0m;
 
+        var chunk = new List<CumulativeTrade>();
+
         for (var i = _allCumulativeTrades.Count - 1; i >= 0; i--)
         {
             var trade = _allCumulativeTrades[i];
@@ -2287,6 +2289,12 @@ public class ClusterStatistic : Indicator
                 break;
 
             if (trade.Time <= now)
+                chunk.Add(trade);
+        }
+
+        chunk.Reverse();
+
+        foreach (var trade in chunk)
             {
                 var delta = trade.Direction == TradeDirection.Buy
                     ? trade.Volume
@@ -2302,7 +2310,6 @@ public class ClusterStatistic : Indicator
                 _winVol += trade.Volume;
                 _winDelta += delta;
             }
-        }
 
         while (_win.Count > 0 && _win.Peek().T <= cutoff)
         {
