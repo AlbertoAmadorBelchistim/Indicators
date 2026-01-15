29c05f0c241367354bf0f1db5b77bb3af9919274
AlbertoAmadorBelchistim <belchistim@gmail.com>
2026-01-15 11:50:57 +0100

    fix(multimarketpower): remove double-replay in history calculation
    After 86ea4948 introduced ReplayBufferedRealtimeAfterHistory, the inline
    replay loops in CalculateHistory became redundant and caused realtime
    trades received during history load to be processed twice.

--- Technical/MultiMarketPower.cs
+++ Technical/MultiMarketPower.cs
@@ -866,9 +866,6 @@ public class MultiMarketPower : Indicator
 				
 				for (var i = _sessionBegin; i <= CurrentBar - 1; i++)
 					CalculateBarTrades(trades, i, ref searchIdx);
-
-				foreach (var trade in _trades)
-					CalculateTrade(trade, false, false);
 			}
 			else
 			{
@@ -879,9 +876,6 @@ public class MultiMarketPower : Indicator
 
 				for (var i = _sessionBegin; i <= CurrentBar - 1; i++)
 					CalculateBarTicks(ticks, i, ref searchIdx);
-
-				foreach (var tick in _ticks)
-					CalculateTick(tick);
 			}
 
 			RedrawChart();
