d7b2b069953abee78ac927678ed5e2e1b22dce29
ErtyHackward <hackward@gmail.com>
2026-05-05 00:11:28 +0100

    PLAT-4067 Add heatmap OHLC Plus indicator

--- Technical/Heatmap/HeatmapIndicatorSettings.cs
+++ Technical/Heatmap/HeatmapIndicatorSettings.cs
@@ -3,12 +3,13 @@
 namespace ATAS.Indicators.Technical.Heatmap;
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using ATAS.Indicators.Heatmap;
+using ATAS.Indicators;
+using ATAS.Indicators.Technical;
 using OFT.Localization;
 using OFT.Rendering.Heatmap;
+using OFT.Rendering.Settings;
 using static ATAS.Indicators.Heatmap.HeatmapIndicatorColors;
 
 #region Enums
@@ -49,22 +50,6 @@ public enum HeatmapPressurePreset
 	Long
 }
 
-public enum HeatmapOhlcLevelKind
-{
-	CurrentDayHigh,
-	CurrentDayLow,
-	CurrentDayOpen,
-	PreviousDayHigh,
-	PreviousDayLow,
-	PreviousDayClose,
-	CurrentWeekHigh,
-	CurrentWeekLow,
-	CurrentWeekOpen,
-	PreviousWeekHigh,
-	PreviousWeekLow,
-	PreviousWeekClose
-}
-
 public enum HeatmapProfileScope
 {
 	DataStart,
@@ -77,39 +62,7 @@ public enum HeatmapProfileScope
 	Contract
 }
 #endregion
-#region OHLC Level entry
-
-/// <summary>
-/// Single OHLC level entry inside <see cref="HeatmapOhlcLevelsSettings"/>.
-/// </summary>
-public readonly record struct HeatmapOhlcLevelSettings(
-	string Id,
-	HeatmapOhlcLevelKind Kind,
-	string Label,
-	HeatmapIndicatorVisualStyle? Style);
-
-#endregion
 #region Settings DTOs
-
-public sealed class HeatmapVwapSettings
-{
-	#region Auto properties
-
-	/// <summary>Schema version. Increment when changing field semantics.</summary>
-	[Browsable(false)]
-	public int Version { get; set; } = 1;
-
-	[Display(ResourceType = typeof(Strings), Name = nameof(Strings.HeatmapVwapInterval))]
-	public HeatmapProfileScope Scope { get; set; } = HeatmapProfileScope.CurrentDay;
-
-	[Display(ResourceType = typeof(Strings), Name = nameof(Strings.HeatmapVwapLineColor))]
-	public CrossColor Color { get; set; } = ParseHex("#FF2962FF");
-
-	[Display(ResourceType = typeof(Strings), Name = nameof(Strings.HeatmapVwapLineThickness))]
-	public float Thickness { get; set; } = 2f;
-
-	#endregion
-}
 
 public sealed class HeatmapValueAreaSettings
 {
@@ -202,52 +155,6 @@ public sealed class HeatmapPressureSettings
 
 	[Display(Name = "Threshold")]
 	public int Threshold { get; set; } = 80;
-
-	#endregion
-}
-
-public sealed class HeatmapOhlcLevelsSettings
-{
-	#region Auto properties
-
-	/// <summary>Schema version. Increment when changing field semantics.</summary>
-	[Browsable(false)]
-	public int Version { get; set; } = 1;
-
-	[Display(Name = "Levels")]
-	public List<HeatmapOhlcLevelSettings> Levels { get; set; } = CreateDefaultLevels();
-
-	#endregion
-
-	#region Public methods
-
-	public static List<HeatmapOhlcLevelSettings> CreateDefaultLevels() =>
-	[
-		new("currentDayHigh", HeatmapOhlcLevelKind.CurrentDayHigh, "Current Day High",
-			new HeatmapIndicatorVisualStyle(Color: "#FF26A69A", Thickness: 1.5f)),
-		new("currentDayLow", HeatmapOhlcLevelKind.CurrentDayLow, "Current Day Low",
-			new HeatmapIndicatorVisualStyle(Color: "#FFEF5350", Thickness: 1.5f)),
-		new("currentDayOpen", HeatmapOhlcLevelKind.CurrentDayOpen, "Current Day Open",
-			new HeatmapIndicatorVisualStyle(Color: "#FFFFEE58", Thickness: 1.5f)),
-		new("previousDayHigh", HeatmapOhlcLevelKind.PreviousDayHigh, "Previous Day High",
-			new HeatmapIndicatorVisualStyle(Color: "#8026A69A", Thickness: 1f)),
-		new("previousDayLow", HeatmapOhlcLevelKind.PreviousDayLow, "Previous Day Low",
-			new HeatmapIndicatorVisualStyle(Color: "#80EF5350", Thickness: 1f)),
-		new("previousDayClose", HeatmapOhlcLevelKind.PreviousDayClose, "Previous Day Close",
-			new HeatmapIndicatorVisualStyle(Color: "#80FFEE58", Thickness: 1f)),
-		new("currentWeekHigh", HeatmapOhlcLevelKind.CurrentWeekHigh, "Current Week High",
-			new HeatmapIndicatorVisualStyle(Color: "#FF42A5F5", Thickness: 1.5f)),
-		new("currentWeekLow", HeatmapOhlcLevelKind.CurrentWeekLow, "Current Week Low",
-			new HeatmapIndicatorVisualStyle(Color: "#FFAB47BC", Thickness: 1.5f)),
-		new("currentWeekOpen", HeatmapOhlcLevelKind.CurrentWeekOpen, "Current Week Open",
-			new HeatmapIndicatorVisualStyle(Color: "#FF78909C", Thickness: 1.5f)),
-		new("previousWeekHigh", HeatmapOhlcLevelKind.PreviousWeekHigh, "Previous Week High",
-			new HeatmapIndicatorVisualStyle(Color: "#8042A5F5", Thickness: 1f)),
-		new("previousWeekLow", HeatmapOhlcLevelKind.PreviousWeekLow, "Previous Week Low",
-			new HeatmapIndicatorVisualStyle(Color: "#80AB47BC", Thickness: 1f)),
-		new("previousWeekClose", HeatmapOhlcLevelKind.PreviousWeekClose, "Previous Week Close",
-			new HeatmapIndicatorVisualStyle(Color: "#8078909C", Thickness: 1f)),
-	];
 
 	#endregion
 }
