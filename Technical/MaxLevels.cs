71253e4771eeccf12549b90d9fc70fe2ad7eeaca
Stig Redtenbacher <stan422@gmail.com>
2026-06-02 20:45:39 +0700

    Revert "PLAT-1189 MaxLevels replay issues fix"
    This reverts commit 81ad55e941f07156d7beb7a0f3699f05b78298dd.

--- Technical/MaxLevels.cs
+++ Technical/MaxLevels.cs
@@ -14,10 +14,7 @@ namespace ATAS.Indicators.Technical
     using OFT.Localization;
     using OFT.Rendering.Context;
     using OFT.Rendering.Tools;
-
-	using Utils.Common.Logging;
-
-	[DisplayName("Maximum Levels")]
+    [DisplayName("Maximum Levels")]
 	[Category(IndicatorCategories.VolumeOrderFlow)]
     [Display(ResourceType = typeof(Strings), Description = nameof(Strings.MaxLevelsIndDescription))]
     [HelpLink("https://help.atas.net/support/solutions/articles/72000602426")]
@@ -260,14 +257,14 @@ namespace ATAS.Indicators.Technical
 			if (bar != CurrentBar - 1)
 				return;
 
-			var candle = GetCandle(bar);
-
 			if (!_candleRequested)
 			{
 				_candleRequested = true;
+				GetFixedProfile(new FixedProfileRequest(Period, TradingSession));
 				_lastSession = bar;
-				RequestProfileAsync(candle.LastTime);
 			}
+
+			var candle = GetCandle(bar);
 
 			if (UseAlert && _lastAlert != bar && _candle is not null)
 			{
@@ -285,22 +282,10 @@ namespace ATAS.Indicators.Technical
 			_prevClose = candle.Close;
 		}
 
-		private async void RequestProfileAsync(DateTime baseTime)
+		protected override void OnFixedProfilesResponse(IndicatorCandle fixedProfileScaled, IndicatorCandle fixedProfileOriginScale, FixedProfilePeriods period)
 		{
-			try
-			{
-				var response = await RequestFixedProfileAsync(new FixedProfileRequest(Period, TradingSession, baseTime));
-
-				if (response is { } r)
-				{
-					_candle = r.Original;
-					RedrawChart();
-				}
-			}
-			catch (Exception ex)
-			{
-				this.LogError(ex.Message, ex);
-			}
+			_candle = fixedProfileOriginScale;
+			RedrawChart();
 		}
 
 		protected override void OnRender(RenderContext context, DrawingLayouts layout)
